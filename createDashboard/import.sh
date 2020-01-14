fffea684a7c65b087e6792e13cf63b7c0df302bb
lili-altibase <43834873+lili-altibase@users.noreply.github.com>
2020-01-14 16:38:00 +0900

    modify argument

--- createDashboard/import.sh
+++ createDashboard/import.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
- 
+
+grafanaip=`echo $1`
+grafanaport=`echo $2`
+influxdbip=`echo $3`
+influxdbport=`echo $4`
+
 #create your datasource
-curl --user admin:admin 'http://$1:$2/api/datasources' -X  POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"InfluxDB","type":"influxdb","url":"http://$3:$4","access":"proxy","database":"telegraf","isDefault":true,"basicAuth":false}'
- 
-#create API key 
-apikey=`curl -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' http://admin:admin@$1:$2/api/auth/keys | jq .key | tr -d '"'`
- 
+curl --user admin:admin 'http://'''$grafanaip''':'''$grafanaport'''/api/datasources' -X  POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"InfluxDB","type":"influxdb","url":"http://'''${influxdbip}''':'''${influxdbport}'''","access":"proxy","database":"telegraf","isDefault":true,"basicAuth":false}'
+
+#create API key
+apikey=`curl -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl", "role": "Admin"}' http://admin:admin@''$grafanaip'':''$grafanaport''/api/auth/keys | jq .key | tr -d '"'`
+
 # import contents
 for i in `ls *.json`;
 do
 #echo ${i}
-    curl -X POST http://$1:$2/api/dashboards/db --insecure -H "Authorization: Bearer ${apikey}" -H "Content-Type:application/json" -d @$i;
+    curl -X POST http://''$grafanaip'':''$grafanaport''/api/dashboards/db --insecure -H "Authorization: Bearer ${apikey}" -H "Content-Type:application/json" -d @$i;
 done
